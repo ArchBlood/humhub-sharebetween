4d6b42bb9d17caa93725f3863ddc077f8a96eb4e
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-06-14 14:37:17 -0400

    Enh: PHP 7.2+

--- config.php
+++ config.php
@@ -3,14 +3,16 @@
 //use humhub\modules\content\widgets\WallEntryControls;
 use humhub\modules\content\components\ContentActiveRecord;
 use humhub\modules\content\widgets\WallEntryLinks;
+use humhub\modules\sharebetween\Module;
+use humhub\modules\sharebetween\Events;
 
 return [
     'id' => 'sharebetween',
-    'class' => 'humhub\modules\sharebetween\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\sharebetween',
     'events' => [
-        ['class' => ContentActiveRecord::className(), 'event' => ContentActiveRecord::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\sharebetween\Events', 'onContentDelete']],
-//        ['class' => WallEntryControls::className(), 'event' => WallEntryControls::EVENT_INIT, 'callback' => ['humhub\modules\sharebetween\Events', 'onWallEntryControlsInit']],
-    	['class' => WallEntryLinks::className(), 'event' => WallEntryLinks::EVENT_INIT, 'callback' => array('humhub\modules\sharebetween\Events', 'onWallEntryLinksInit')],
+        ['class' => ContentActiveRecord::class, 'event' => ContentActiveRecord::EVENT_BEFORE_DELETE, 'callback' => [Events::class, 'onContentDelete']],
+//        ['class' => WallEntryControls::class, 'event' => WallEntryControls::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryControlsInit']],
+    	['class' => WallEntryLinks::class, 'event' => WallEntryLinks::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryLinksInit']],
      ],
 ];
