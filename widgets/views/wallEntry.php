14a43cefdfb96b86c9bc9c63eb11439afe6c03b1
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-06-14 15:37:48 -0400

    Fix: Don't mix short PHP tags and long tags

--- widgets/views/wallEntry.php
+++ widgets/views/wallEntry.php
@@ -18,23 +18,21 @@ $addonOptions = [
 ];
 ?>
 
-<div class="panel panel-default wall_shared_content wall_<?php echo $object->getUniqueId(); ?>">
+<div class="panel panel-default wall_shared_content wall_<?= $object->getUniqueId(); ?>">
     <div class="panel-body">
         <div class="media">
-            <?php echo \humhub\modules\content\widgets\WallEntryControls::widget(['object' => $object, 'wallEntryWidget' => $wallEntryWidget]); ?>
+            <?= WallEntryControls::widget(['object' => $object, 'wallEntryWidget' => $wallEntryWidget]); ?>
 
             <p>
                 <?= Yii::t('SharebetweenModule.base', '{displayName} shared a {contentType}.', ['displayName' => Html::a($user->displayName, $user->getUrl(), ['style' => 'color: #e5c150']), 'contentType' => Html::a($sharedContent->getContentName(), $sharedContent->content->getUrl())]); ?>
             </p>
 
-            <div class="content wall-content" id="wall_content_<?php echo $object->getUniqueId(); ?>">
-                <?php echo $content; ?>
+            <div class="content wall-content" id="wall_content_<?= $object->getUniqueId(); ?>">
+                <?= $content; ?>
             </div>
 
             <div class="stream-entry-addons">
-                <?php
-                echo WallEntryAddons::widget($addonOptions);
-                ?>
+                <?= WallEntryAddons::widget($addonOptions); ?>
             </div>
         </div>
     </div>
